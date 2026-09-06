77a64f05a4f3d0a6f7a041228c2c2644c0fe056a
Rahulmurai <96008084+Rahulmurai@users.noreply.github.com>
2026-09-06 13:36:40 +0530

    Add reusable e-commerce test data

---
+++ ECommerce-Playwright-Framework/test-data/users.ts
@@ -0,0 +1,22 @@
+export const users = {
+  valid: {
+    username: 'standard_user',
+    password: 'secret_sauce'
+  },
+  locked: {
+    username: 'locked_out_user',
+    password: 'secret_sauce'
+  }
+};
+
+export const checkoutData = {
+  firstName: 'Rahul',
+  lastName: 'Murai',
+  postalCode: '411001'
+};
+
+export const products = {
+  backpack: 'Sauce Labs Backpack',
+  bikeLight: 'Sauce Labs Bike Light',
+  fleeceJacket: 'Sauce Labs Fleece Jacket'
+};
